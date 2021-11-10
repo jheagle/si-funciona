266f4b00fef24d9236c2c6709bc8b4e98e75800b
Joshua Heagle <joshuaheagle@gmail.com>
2021-11-10 13:15:50 -0500

    Simplify testHelpers export

--- tests/testHelpers.js
+++ tests/testHelpers.js
@@ -140,14 +140,3 @@ export const nodeTree = { name: 'one', parent: null, children: [] }
 nodeTree.children[0] = { name: 'child one', parent: nodeTree, children: [] }
 nodeTree.children[1] = { name: 'child two', parent: nodeTree, children: [] }
 nodeTree.children[0].children[0] = { name: 'grandchild one', parent: nodeTree.children[0], children: [] }
-
-const root = this || (typeof window !== 'undefined') ? window : {}
-root.testHelpers = {
-  logObject,
-  circularObject,
-  deepReferenceObject,
-  linkedList,
-  jsonDom,
-  domItem,
-  multiReferenceObject
-}
