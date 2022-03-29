6c38304423948b702c44bfb5f84a5b9c157a91b3
Joshua Heagle <joshuaheagle@gmail.com>
2022-03-29 12:38:48 -0400

    - Import babel.config from build-tools package.

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,1 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      useBuiltIns: 'usage',
-      corejs: { version: '3.6', proposals: true }
-    }]
-  ]
-}
+module.exports = require('js-build-tools/babel.config')
